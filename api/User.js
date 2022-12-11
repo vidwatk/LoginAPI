c8b1ff761a8baf9b15ffef5419f1e6a7b6bf7c00
Vidwat Kanade <vidwatk@gmail.com>
2022-12-11 15:59:33 +0000

    user data validation

--- api/User.js
+++ api/User.js
@@ -3,7 +3,23 @@ const router  = express.Router();
 
 
 router.post("./register", (req,res) => {
+    let {name, email, password, dateOfBirth, phoneNumber} = req.body;
+    name = name.trim();
+    email = email.trim()
+    password = password.trim();
+    dateOfBirth = dateOfBirth.trim();
+    phoneNumber = phoneNumber.trim();
 
+    if (name == "" || email == "" || password == "" || dateOfBirth == "" || phoneNumber == "") {
+        res.json({
+            status: "FAILED",
+            message: "Do not leave any input fields empty"
+        });
+    }else if (!/^([^\p{N}\p{S}\p{C}\\\/]{2,20})$/.test(name)) {                          //https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
+        res.json({
+            status: "FAILED, does not pass the validation. Check the front end for more info"
+        })
+    }
 })
 
 router.post("./login", (req,res) => {
--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
     email: String,
     password: String,
     dateOfBirth: Date,
+    phoneNumber: Number
 
 })
 
